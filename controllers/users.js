a52068a03d7488e714e49854588547f0e6ea5b31
yala0 <welcome.yanlafitte@gmail.com>
2022-07-10 20:19:41 +0200

    modified error

--- controllers/users.js
+++ controllers/users.js
@@ -62,5 +62,5 @@ exports.login = (req, res, next) => {
         })
         .catch((error) => res.staus(500).json({ error }));
     })
-    .catch((error) => res.staus(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));
 };
